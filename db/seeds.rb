06c0109f57502c489cb085d393408d4b27d802b7
cloteau camille <camille.cloteau@gmail.com>
2022-02-18 13:58:15 +0100

    new button to commeback with lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,16 @@ puts "creation de list"
 10.times do
 
 
-  # list = %w[drama comedie marvel guerre space ScienceFiction pirate americain ghibli].sample
-  # listing = List.new(name: list)
-  # listing.save
+  list = %w[drama comedie marvel guerre space ScienceFiction pirate americain ghibli].sample
+  listing = List.new(name: list)
+  listing.save
 
 
 
-  # rating = rand(0.1..4.9)
+  rating = rand(0.1..4.9)
 
-  # movie = Movie.new(title: Faker::Movie.title, overview: "ceci est le meilleur film du monde, il y a rien d'autre a dire", poster_url: Faker::Internet.url, rating: rating)
-  # movie.save
+  movie = Movie.new(title: Faker::Movie.title, overview: "ceci est le meilleur film du monde, il y a rien d'autre a dire", poster_url: Faker::Internet.url, rating: rating)
+  movie.save
 
 end
 puts " list Save"
